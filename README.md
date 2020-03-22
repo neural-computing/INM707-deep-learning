513b19079fc680e9051dbfa8e1489e2f906750e3
tpgmartin <tpgmartin@gmail.com>
2020-03-22 23:43:41 +0000

    Include Open AI retro and baseline projects

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # INM707 Deep Learning
+
+## Installation
+
+This project requires [OpenAI Baselines](https://github.com/openai/baselines). Follow installation steps in linked project - can skip installation of tensorflow as this is already included in the project dependencies.
--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-import argparse
-import torch
+# import argparse
+# import torch
 
-parser = argparse.ArgumentParser(description='Set hyperparameters')
-parser.add_argument('--lr', default=0.0001, help='Learning Rate')
-parser.add_argument('--gamma', default=0.99, help='TODO')
-parser.add_argument('--tau', default=1, help='TODO')
-parser.add_argument('--seed', default=1, help='TODO')
-parser.add_argument('--num_processes', default=16, help='TODO')
-parser.add_argument('--num_steps', default=20, help='TODO')
-parser.add_argument('--max_epoch', default=10000, help='TODO')
-parser.add_argument('--env', default='ENV_NAME', help='TODO')
+# parser = argparse.ArgumentParser(description='Set hyperparameters')
+# parser.add_argument('--lr', default=0.0001, help='Learning Rate')
+# parser.add_argument('--gamma', default=0.99, help='TODO')
+# parser.add_argument('--tau', default=1, help='TODO')
+# parser.add_argument('--seed', default=1, help='TODO')
+# parser.add_argument('--num_processes', default=16, help='TODO')
+# parser.add_argument('--num_steps', default=20, help='TODO')
+# parser.add_argument('--max_epoch', default=10000, help='TODO')
+# parser.add_argument('--env', default='ENV_NAME', help='TODO')
+
+# if __name__ == "__main__":
+
+#     # Set hyperparameters
+#     params = parser.parse_args()
+
+#     # Set seed
+#     torch.manual_seed(params.seed)
+
+#     # Create environment
+
+import retro
+
+def main():
+    env = retro.make(game='Airstriker-Genesis')
+    obs = env.reset()
+    while True:
+        obs, rew, done, info = env.step(env.action_space.sample())
+        env.render()
+        if done:
+            obs = env.reset()
+    env.close()
+
 
 if __name__ == "__main__":
-
-    # Set hyperparameters
-    params = parser.parse_args()
-
-    # Set seed
-    torch.manual_seed(params.seed)
-
-    # Create environment
+    main()
